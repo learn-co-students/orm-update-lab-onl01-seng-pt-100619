9d2434fabed66489d391521f44a76d5bbc77928e
Alex Martinez <alexandr3martin3z@gmail.com>
2019-12-20 05:15:30 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
       SQL
 
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
 
   end
@@ -49,11 +49,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
-    student
+    self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
